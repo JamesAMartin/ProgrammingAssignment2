38ba97b4f66859b80f44fb2c685db18f00e417b4
Jim Martin <jmartin@ccli.com>
2015-11-10 13:36:09 -0800

    Setup an R project for this assignment and performed some basic reformatting of the initial code structure.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,19 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+
+
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function( x = matrix() )
+{
 
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...)
+{
         ## Return a matrix that is the inverse of 'x'
 }
